8bbe49e6f6ddeb9ea77ef125bc6eb44046ecae45
Nimil Shah <nimil.shah@infostretch.com>
2021-05-26 11:32:09 +0530

    initial commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
-
-
-mongoose.connect(process.env.MONGODB_URL,{
-useNewUrlParser : true,
-useCreateIndex: true,
-useFindAndModify:true
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
 
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT
-
-
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
@@ -16,3 +14,14 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
+}
+
+myFunction()
